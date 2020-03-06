b0fc92ee1b551632c3c8778928e6d1c0ea6ec0dc
Full stack developer <53688893+creditcoder@users.noreply.github.com>
2020-03-05 23:23:13 -0800

    fix location bug, profile avatar bug, change assets

--- src/Style.ts
+++ src/Style.ts
@@ -4,7 +4,7 @@ import {Colors} from 'src/Theme';
 export default StyleSheet.create({
   FindStuffHeaderContainer: {
     width: Dimensions.get('window').width,
-    height: Platform.OS === 'android' ? 40 : 80,
+    height: Platform.OS === 'android' ? 40 : 55,
     backgroundColor: '#0084da',
     alignItems: 'flex-end',
     paddingBottom: 5,
