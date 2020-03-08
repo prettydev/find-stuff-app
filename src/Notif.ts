252df1405e73c2a1a6f95c1e39999c5b6bf3da39
Full stack developer <53688893+creditcoder@users.noreply.github.com>
2020-03-07 17:49:24 -0800

    fix ios notif bug

--- src/Notif.ts
+++ src/Notif.ts
@@ -65,15 +65,15 @@ export const localNotifTitle = (title, data) => {
 
     /* iOS only properties */
     alertAction: 'view', // (optional) default: view
-    category: null, // (optional) default: null
-    userInfo: null, // (optional) default: null (object containing additional notification data)
+    category: '', // (optional) default: null
+    userInfo: {}, // (optional) default: null (object containing additional notification data)
 
     /* iOS and Android properties */
     title, //data.alarm.id + '', // (optional)
     message: data, //data.alarm.wellName, // (required)
     // playSound: false, // (optional) default: true
     soundName: 'default', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
-    number: '10', // (optional) Valid 32 bit integer specified as string. default: none (Cannot be zero)
+    // number: 10, // (optional) Valid 32 bit integer specified as string. default: none (Cannot be zero)
     //actions: '["Yes", "No"]',  // (Android only) See the doc for notification actions to know more
   });
   setTimeout(() => {
